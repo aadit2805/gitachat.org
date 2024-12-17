87b1361528e36a088455bc7986a45c947c841ab8
Aadit Shah <aadit2805@gmail.com>
2024-12-17 00:48:28 -0600

    fixed git ignore and url for deploy

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from model import match
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], 
+    allow_origins=["http://localhost:3000", "https://gitachat.org"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -16,12 +20,16 @@ app.add_middleware(
 class Query(BaseModel):
     query: str
 
-@app.post("/api/query")
-async def query_gita(query: Query):
+@app.post("/api/query", response_model=dict)
+async def query_gita(query: Query) -> dict:
+    """
+    Query the Gita with the provided query string(s).
+    """
     try:
         result = match(query.query)
-        if not result:  # Assuming empty result is invalid
+        if not result:  
             raise HTTPException(status_code=404, detail="No matches found")
         return {"status": "success", "data": result}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        logging.error(f"Error occurred: {str(e)}")  
+        raise HTTPException(status_code=500, detail="Internal Server Error")
